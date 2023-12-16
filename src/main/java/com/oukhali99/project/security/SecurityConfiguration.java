a2c362a8390b4ba2753d2b97f437b2b9c9469d69
Oussama Khalifeh <oussamak@PC-KIWI>
2023-12-16 18:04:08 -0500

    Disable cors for localhost:3000

--- src/main/java/com/oukhali99/project/security/SecurityConfiguration.java
+++ src/main/java/com/oukhali99/project/security/SecurityConfiguration.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.web.configurers.SessionMan
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -55,5 +57,16 @@ public class SecurityConfiguration {
 
         return httpSecurity.build();
     }
+
+    @Bean
+    public WebMvcConfigurer corsMappingConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
 
 }
