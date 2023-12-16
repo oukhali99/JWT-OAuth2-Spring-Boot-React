3c939910d9e0c28f73f400ba51f33680d965e41b
Oussama Khalifeh <oussamak@PC-KIWI>
2023-12-15 19:09:58 -0500

    First commit

---
+++ src/test/java/com/oukhali99/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oukhali99.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
