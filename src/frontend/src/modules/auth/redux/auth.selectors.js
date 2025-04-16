1082c2168a5367a66c04ce1afb7e1762753c0729
Oussama Khalifeh <oukhali@hotmail.com>
2025-04-15 21:48:04 -0400

    Converted some files to Typescript

--- src/frontend/src/modules/auth/redux/auth.selectors.js
+++
@@ -1,3 +0,0 @@
-export const getToken = (state) => state.auth.token;
-export const getUsername = (state) => state.auth.username;
-export const getId = (state) => state.auth.id;
--- src/frontend/src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import modules from "./modules";
-
-export default configureStore({
-    reducer: modules,
-});
